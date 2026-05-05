3103d78fb5048124f280edb43f75db66396a62d5
Harshithaj2001 <your-github-email>
2026-05-05 16:18:11 -0400

    TVR Biryani Gramam website

---
+++ menu.js
@@ -0,0 +1,194 @@
+const menu = {
+  "Non Veg Starters": [
+    {id:"nvs1",name:"Crispy Wings",price:250,veg:false},{id:"nvs2",name:"Chilly Wings",price:260,veg:false},
+    {id:"nvs3",name:"Chicken Fry Bone",price:240,veg:false},{id:"nvs4",name:"Chicken Fry B/L",price:270,veg:false},
+    {id:"nvs5",name:"Chilly Chicken B/L",price:270,veg:false},{id:"nvs6",name:"Ginger Chicken",price:280,veg:false},
+    {id:"nvs7",name:"Chicken 65",price:280,veg:false},{id:"nvs8",name:"Chicken Manchurian",price:280,veg:false},
+    {id:"nvs9",name:"Fride Wings",price:270,veg:false},{id:"nvs10",name:"Crispy Lolypop (6 Pcs)",price:270,veg:false},
+    {id:"nvs11",name:"Chilly Lolypop (6 Pcs)",price:280,veg:false},{id:"nvs12",name:"Chicken 555",price:310,veg:false},
+    {id:"nvs13",name:"Chicken Majistick",price:310,veg:false},{id:"nvs14",name:"Chicken Sangirella",price:320,veg:false},
+    {id:"nvs15",name:"Chicken 85 Bone",price:280,veg:false},{id:"nvs16",name:"Valcon Chicken",price:310,veg:false},
+    {id:"nvs17",name:"Gulzara Chicken",price:320,veg:false},{id:"nvs18",name:"Hongkong Chicken",price:300,veg:false},
+    {id:"nvs19",name:"Dragon Chicken",price:310,veg:false},{id:"nvs20",name:"Chicken Lemon",price:300,veg:false},
+    {id:"nvs21",name:"Chicken Thai Chi",price:300,veg:false},{id:"nvs22",name:"Kaju Nut Chicken",price:320,veg:false},
+    {id:"nvs23",name:"Chicken Thai Pai Chi",price:300,veg:false},{id:"nvs24",name:"Spicy Fried Chicken",price:310,veg:false},
+    {id:"nvs25",name:"Crispy Chicken",price:300,veg:false},{id:"nvs26",name:"Salt & Pepper Chicken",price:300,veg:false},
+    {id:"nvs27",name:"Chicken Satay",price:300,veg:false},{id:"nvs28",name:"Stick Chicken",price:340,veg:false},
+    {id:"nvs29",name:"Red Hot Chicken",price:300,veg:false},{id:"nvs30",name:"Pepper Chicken",price:290,veg:false},
+    {id:"nvs31",name:"Chilly Egg",price:190,veg:false},{id:"nvs32",name:"Egg 65",price:200,veg:false},
+    {id:"nvs33",name:"Boiled Egg (2 Piece)",price:60,veg:false},{id:"nvs34",name:"KFC Wings",price:300,veg:false}
+  ],
+  "Veg Starters": [
+    {id:"vs1",name:"Chilly Garlic Potato",price:180,veg:true},{id:"vs2",name:"Veg Manchurian Dry",price:200,veg:true},
+    {id:"vs3",name:"Veg Manchurian Wet",price:220,veg:true},{id:"vs4",name:"Crispy Veg",price:200,veg:true},
+    {id:"vs5",name:"Crispy Corn",price:230,veg:true},{id:"vs6",name:"Chilly Mushroom",price:260,veg:true},
+    {id:"vs7",name:"Mushroom Manchurian",price:260,veg:true},{id:"vs8",name:"Mushroom 65",price:270,veg:true},
+    {id:"vs9",name:"Salt & Pepper Mushroom",price:280,veg:true},{id:"vs10",name:"Paneer 65",price:270,veg:true},
+    {id:"vs11",name:"Chilly Paneer",price:270,veg:true},{id:"vs12",name:"Paneer Manchurian",price:270,veg:true},
+    {id:"vs13",name:"Paneer Majistic",price:260,veg:true},{id:"vs14",name:"Paneer Salt & Pepper",price:280,veg:true},
+    {id:"vs15",name:"Paneer Dostana",price:270,veg:true},{id:"vs16",name:"Paneer Satay",price:270,veg:true},
+    {id:"vs17",name:"Chilly Baby Corn",price:260,veg:true},{id:"vs18",name:"Red Pepper Baby Corn",price:270,veg:true},
+    {id:"vs19",name:"Crispy Baby Corn",price:270,veg:true},{id:"vs20",name:"Baby Corn Manchurian",price:260,veg:true},
+    {id:"vs21",name:"Baby Corn 65",price:280,veg:true},{id:"vs22",name:"Honey Chilly Potato",price:250,veg:true},
+    {id:"vs23",name:"Veg Sangai",price:270,veg:true},{id:"vs24",name:"Dragon Veg",price:260,veg:true},
+    {id:"vs25",name:"Golden Fired Baby Corn",price:270,veg:true},{id:"vs26",name:"Odyssy Veg",price:270,veg:true}
+  ],
+  "Non Veg Main Course": [
+    {id:"nvm1",name:"Chicken Curry Bone",price:240,veg:false},{id:"nvm2",name:"Chicken Curry B/L",price:270,veg:false},
+    {id:"nvm3",name:"Chicken Fry Bone",price:260,veg:false},{id:"nvm4",name:"Kalmi Masala (Half)",price:300,veg:false},
+    {id:"nvm5",name:"Chicken Masala Bone",price:250,veg:false},{id:"nvm6",name:"Chicken Mughlai",price:280,veg:false},
+    {id:"nvm7",name:"Chicken Masala B/L",price:280,veg:false},{id:"nvm8",name:"Kadai Chicken B/L",price:280,veg:false},
+    {id:"nvm9",name:"Chicken Chittinadu",price:290,veg:false},{id:"nvm10",name:"Chicken Maharani B/L",price:310,veg:false},
+    {id:"nvm11",name:"Butter Chicken B/L",price:280,veg:false},{id:"nvm12",name:"Chicken Tikka Masala B/L",price:300,veg:false},
+    {id:"nvm13",name:"Afgani Murgi Masala",price:300,veg:false},{id:"nvm14",name:"Mutton Curry",price:340,veg:false},
+    {id:"nvm15",name:"Mutton Masala",price:350,veg:false},{id:"nvm16",name:"Tandoori Masala (Half)",price:300,veg:false},
+    {id:"nvm17",name:"Kalmi Masala (Full)",price:420,veg:false},{id:"nvm18",name:"Tandoori Masala (Full)",price:580,veg:false}
+  ],
+  "Veg Main Course": [
+    {id:"vm1",name:"Baby Corn Curry",price:240,veg:true},{id:"vm2",name:"Baby Corn Masala",price:240,veg:true},
+    {id:"vm3",name:"Mixed Veg Curry",price:220,veg:true},{id:"vm4",name:"Veg Chat Pat",price:220,veg:true},
+    {id:"vm5",name:"Paneer Butter Masala",price:260,veg:true},{id:"vm6",name:"Paneer Curry",price:250,veg:true},
+    {id:"vm7",name:"Mushroom Curry",price:250,veg:true},{id:"vm8",name:"Mushroom Masala",price:250,veg:true},
+    {id:"vm9",name:"Kadai Baby Corn",price:260,veg:true},{id:"vm10",name:"Kadai Mushroom",price:260,veg:true},
+    {id:"vm11",name:"Kadai Paneer",price:260,veg:true},{id:"vm12",name:"Paneer Methi Chaman",price:280,veg:true},
+    {id:"vm13",name:"Kaju Tomato Curry",price:270,veg:true},{id:"vm14",name:"Palak Paneer",price:280,veg:true},
+    {id:"vm15",name:"Kaju Paneer",price:300,veg:true},{id:"vm16",name:"Kaju Mushroom",price:300,veg:true},
+    {id:"vm17",name:"Kaju Baby Corn",price:300,veg:true},{id:"vm18",name:"Paneer Tikka Masala",price:310,veg:true},
+    {id:"vm19",name:"Dal Fry",price:180,veg:true},{id:"vm20",name:"Tomato Dal",price:190,veg:true}
+  ],
+  "Non Veg Biryanis": [
+    {id:"nvb1",name:"Biryani Rice (Non-Veg)",price:200,veg:false},{id:"nvb2",name:"Egg Biryani",price:220,veg:false},
+    {id:"nvb3",name:"Chicken Dum Biryani",price:240,veg:false},{id:"nvb4",name:"Chicken Fry Piece Biryani",price:250,veg:false},
+    {id:"nvb5",name:"Fish Biryani B/L",price:340,veg:false},{id:"nvb6",name:"Chicken Mughalai Biryani",price:340,veg:false},
+    {id:"nvb7",name:"Prawns Biryani",price:350,veg:false},{id:"nvb8",name:"Spl. Chicken Biryani B/L",price:340,veg:false},
+    {id:"nvb9",name:"Mutton Fry Piece Biryani",price:370,veg:false},{id:"nvb10",name:"Lollypop Biryani",price:340,veg:false},
+    {id:"nvb11",name:"Chicken Tikka Biryani",price:370,veg:false},{id:"nvb12",name:"Kalmi Biryani",price:380,veg:false},
+    {id:"nvb13",name:"Chicken Tandoori Biryani",price:480,veg:false},{id:"nvb14",name:"Potlam Biryani",price:490,veg:false},
+    {id:"nvb15",name:"Hyd Dum Joint Biryani",price:260,veg:false},{id:"nvb16",name:"Double Joint Biryani",price:470,veg:false},
+    {id:"nvb17",name:"TVR Mixed Biryani",price:480,veg:false},{id:"nvb18",name:"Family Pack Biryani",price:1099,veg:false}
+  ],
+  "Veg Biryanis": [
+    {id:"vb1",name:"Veg Biryani",price:210,veg:true},{id:"vb2",name:"Paneer Biryani",price:280,veg:true},
+    {id:"vb3",name:"Mushroom Biryani",price:280,veg:true},{id:"vb4",name:"Kaju Biryani",price:300,veg:true},
+    {id:"vb5",name:"Kaju Paneer Biryani",price:310,veg:true},{id:"vb6",name:"Special Veg Biryani",price:320,veg:true},
+    {id:"vb7",name:"Mixed Veg Biryani",price:320,veg:true}
+  ],
+  "TVR Mandis": [
+    {id:"m1",name:"Chicken Fry Mandi (Half)",price:580,veg:false},{id:"m2",name:"Chicken Fry Mandi (Full)",price:1080,veg:false},
+    {id:"m3",name:"Chicken Roast Mandi (Half)",price:600,veg:false},{id:"m4",name:"Chicken Roast Mandi (Full)",price:1100,veg:false},
+    {id:"m5",name:"Fish Mandi (Half)",price:750,veg:false},{id:"m6",name:"Fish Mandi (Full)",price:1400,veg:false},
+    {id:"m7",name:"Mutton Mandi (Half)",price:750,veg:false},{id:"m8",name:"Mutton Mandi (Full)",price:1350,veg:false},
+    {id:"m9",name:"Mutton Juice Mandi (Half)",price:760,veg:false},{id:"m10",name:"Mutton Juice Mandi (Full)",price:1420,veg:false},
+    {id:"m11",name:"Prawns Mandi (Half)",price:700,veg:false},{id:"m12",name:"Prawns Mandi (Full)",price:1200,veg:false},
+    {id:"m13",name:"Tandoori Mandi (Half)",price:650,veg:false},{id:"m14",name:"Tandoori Mandi (Full)",price:1220,veg:false},
+    {id:"m15",name:"Mixed Mandi (Half)",price:900,veg:false},{id:"m16",name:"Mixed Mandi (Full)",price:1600,veg:false}
+  ],
+  "Tandoori": [
+    {id:"t1",name:"Tandoori Chicken Half",price:270,veg:false},{id:"t2",name:"Tandoori Chicken Full",price:500,veg:false},
+    {id:"t3",name:"Kalmi Kabab Half",price:200,veg:false},{id:"t4",name:"Kalmi Kabab Full",price:390,veg:false},
+    {id:"t5",name:"Chicken Tikka",price:300,veg:false},{id:"t6",name:"Reshmi Kabab",price:320,veg:false},
+    {id:"t7",name:"Haryali Chicken Tikka",price:320,veg:false},{id:"t8",name:"Chicken Seekh Kabab",price:320,veg:false},
+    {id:"t9",name:"Murgi Malai Kabab",price:320,veg:false},{id:"t10",name:"Fish Tikka",price:330,veg:false},
+    {id:"t11",name:"Ajwani Fish Tikka",price:340,veg:false},{id:"t12",name:"Acharya Fish Tikka",price:340,veg:false}
+  ],
+  "Tandoori Special Fish": [
+    {id:"tsf1",name:"Tandoori Chanduva",price:320,veg:false},{id:"tsf2",name:"Tandoori Korameenu",price:430,veg:false},
+    {id:"tsf3",name:"Tandoori Vanjaram",price:580,veg:false}
+  ],
+  "Veg Tandoori": [
+    {id:"vt1",name:"Veg Hara Bhara Kabab",price:250,veg:true},{id:"vt2",name:"Paneer Tikka",price:280,veg:true},
+    {id:"vt3",name:"Malai Paneer Tikka",price:290,veg:true},{id:"vt4",name:"Baby Corn Tikka",price:280,veg:true},
+    {id:"vt5",name:"Mushroom Tikka",price:280,veg:true},{id:"vt6",name:"Haryali Paneer Tikka",price:290,veg:true},
+    {id:"vt7",name:"Pudhina Paneer Tikka",price:290,veg:true}
+  ],
+  "Sea Food": [
+    {id:"sf1",name:"Fish Roast Bone",price:250,veg:false},{id:"sf2",name:"Fish Fry Bone",price:260,veg:false},
+    {id:"sf3",name:"Fish B/L",price:300,veg:false},{id:"sf4",name:"Chilly Fish",price:300,veg:false},
+    {id:"sf5",name:"Lemon Fish",price:300,veg:false},{id:"sf6",name:"Apollo Fish B/L",price:300,veg:false},
+    {id:"sf7",name:"Schezwan Fish",price:310,veg:false},{id:"sf8",name:"Fish Fingers",price:320,veg:false},
+    {id:"sf9",name:"Fried Fish",price:320,veg:false},{id:"sf10",name:"Prawn Manchurian",price:320,veg:false},
+    {id:"sf11",name:"Chilly Prawns",price:320,veg:false},{id:"sf12",name:"Schezwan Prawns",price:320,veg:false},
+    {id:"sf13",name:"Salt & Pepper Prawns",price:320,veg:false},{id:"sf14",name:"Prawns Fry",price:320,veg:false},
+    {id:"sf15",name:"Butter Prawns",price:340,veg:false},{id:"sf16",name:"Dunkon Prawns",price:320,veg:false},
+    {id:"sf17",name:"Chilly Honey Prawns",price:320,veg:false},{id:"sf18",name:"Fried Prawns",price:330,veg:false},
+    {id:"sf19",name:"Loose Prawns",price:340,veg:false}
+  ],
+  "Non Veg Curries": [
+    {id:"nvc1",name:"Fish Pulusu",price:250,veg:false},{id:"nvc2",name:"Nelluru Fish Pulusu",price:260,veg:false},
+    {id:"nvc3",name:"Fish Curry B/L",price:290,veg:false},{id:"nvc4",name:"Prawn Curry",price:300,veg:false},
+    {id:"nvc5",name:"Prawns Masala",price:310,veg:false},{id:"nvc6",name:"Gongura Prawns",price:320,veg:false},
+    {id:"nvc7",name:"Kadai Prawns",price:320,veg:false},{id:"nvc8",name:"Prawns Fry",price:320,veg:false},
+    {id:"nvc9",name:"Korameenu Iguru",price:320,veg:false},{id:"nvc10",name:"Natukodi Pulusu",price:350,veg:false},
+    {id:"nvc11",name:"Mutton Rogan Josh",price:340,veg:false},{id:"nvc12",name:"Natukodi Fry",price:360,veg:false},
+    {id:"nvc13",name:"Gongura Mutton",price:360,veg:false},{id:"nvc14",name:"Mutton Fry",price:350,veg:false},
+    {id:"nvc15",name:"Egg Curry",price:160,veg:false},{id:"nvc16",name:"Egg Bhurji",price:150,veg:false}
+  ],
+  "Fried Rice": [
+    {id:"fr1",name:"Veg Fried Rice",price:220,veg:true},{id:"fr2",name:"Spl. Veg Fried Rice",price:320,veg:true},
+    {id:"fr3",name:"Veg Schezwan Fried Rice",price:240,veg:true},{id:"fr4",name:"Paneer Fried Rice",price:260,veg:true},
+    {id:"fr5",name:"Mushroom Fried Rice",price:260,veg:true},{id:"fr6",name:"Kaju Paneer Fried Rice",price:280,veg:true},
+    {id:"fr7",name:"Egg Fried Rice",price:240,veg:false},{id:"fr8",name:"Chicken Fried Rice",price:260,veg:false},
+    {id:"fr9",name:"Schezwan Chicken Fried Rice",price:280,veg:false},{id:"fr10",name:"Spl. Chicken Fried Rice",price:340,veg:false},
+    {id:"fr11",name:"Prawn Fried Rice",price:320,veg:false},{id:"fr12",name:"Mixed Non-Veg Fried Rice",price:360,veg:false},
+    {id:"fr13",name:"Curd Rice",price:120,veg:true},{id:"fr14",name:"White Rice",price:60,veg:true},
+    {id:"fr15",name:"Curd",price:20,veg:true}
+  ],
+  "Noodles": [
+    {id:"n1",name:"American Veg Choupsy",price:180,veg:true},{id:"n2",name:"Veg Soft Noodles",price:150,veg:true},
+    {id:"n3",name:"Veg Hakka Noodles",price:160,veg:true},{id:"n4",name:"Veg Schezwan Noodles",price:170,veg:true},
+    {id:"n5",name:"American Non Veg Choupsy",price:220,veg:false},{id:"n6",name:"Chilly Garlic Noodles",price:180,veg:true},
+    {id:"n7",name:"Egg Noodles",price:180,veg:false},{id:"n8",name:"Chicken Noodles",price:220,veg:false},
+    {id:"n9",name:"Chicken Schezwan Noodles",price:230,veg:false},{id:"n10",name:"Mixed Non Veg Noodles",price:240,veg:false}
+  ],
+  "Soups": [
+    {id:"so1",name:"Veg Tomato Soup",price:100,veg:true},{id:"so2",name:"Veg Sweet Corn Soup",price:100,veg:true},
+    {id:"so3",name:"Veg Hot And Sour Soup",price:120,veg:true},{id:"so4",name:"Veg Lemon Coriander Soup",price:100,veg:true},
+    {id:"so5",name:"Veg Clear Soup",price:100,veg:true},{id:"so6",name:"Veg Tomyum Soup",price:110,veg:true},
+    {id:"so7",name:"Veg Chilly Garlic Soup",price:130,veg:true},{id:"so8",name:"Veg Dragon Soup",price:130,veg:true},
+    {id:"so9",name:"Veg Thai Pepper Soup",price:100,veg:true},{id:"so10",name:"Veg Hakka Noodles Soup",price:100,veg:true},
+    {id:"so11",name:"Veg Cream Mushroom Soup",price:110,veg:true},{id:"so12",name:"Veg Manchow Soup",price:130,veg:true},
+    {id:"so13",name:"Chicken Sweet Corn Soup",price:120,veg:false},{id:"so14",name:"Chicken Hot & Sour Soup",price:130,veg:false},
+    {id:"so15",name:"Chicken Lemon Coriander Soup",price:120,veg:false},{id:"so16",name:"Chicken Clear Soup",price:130,veg:false},
+    {id:"so17",name:"Non Veg Tomyum Soup",price:130,veg:false},{id:"so18",name:"Non Veg Sea Food Soup",price:120,veg:false},
+    {id:"so19",name:"Non Veg Thai Pepper Soup",price:140,veg:false},{id:"so20",name:"Non Veg Hakka Noodles Soup",price:130,veg:false},
+    {id:"so21",name:"Non Veg Lungh Fungh Soup",price:130,veg:false},{id:"so22",name:"Chicken Manchow Soup",price:140,veg:false}
+  ],
+  "Sizzlers": [
+    {id:"sz1",name:"Chicken Sizzler",price:400,veg:false},{id:"sz2",name:"Fish Sizzler",price:400,veg:false},
+    {id:"sz3",name:"Prawns Sizzler",price:450,veg:false}
+  ],
+  "Indian Breads": [
+    {id:"ib1",name:"Pulka",price:20,veg:true},{id:"ib2",name:"Butter Pulka",price:30,veg:true},
+    {id:"ib3",name:"Plain Roti",price:40,veg:true},{id:"ib4",name:"Butter Roti",price:50,veg:true},
+    {id:"ib5",name:"Plain Naan",price:60,veg:true},{id:"ib6",name:"Butter Naan",price:70,veg:true},
+    {id:"ib7",name:"Garlic Naan",price:80,veg:true},{id:"ib8",name:"Folding Naan",price:80,veg:true},
+    {id:"ib9",name:"Masala Kulcha",price:100,veg:true},{id:"ib10",name:"Paneer Kulcha",price:120,veg:true},
+    {id:"ib11",name:"Stuffed Kulcha",price:120,veg:true}
+  ],
+  "Mocktails": [
+    {id:"mc1",name:"Green Apple Mojito",price:110,veg:true},{id:"mc2",name:"Orange Mojito",price:110,veg:true},
+    {id:"mc3",name:"Kiwi Mojito",price:120,veg:true},{id:"mc4",name:"Lime Mint Mojito",price:120,veg:true},
+    {id:"mc5",name:"Mint Mojito",price:120,veg:true},{id:"mc6",name:"Black Berry",price:120,veg:true},
+    {id:"mc7",name:"Orange Lemonade",price:120,veg:true},{id:"mc8",name:"Blue Lagoon",price:120,veg:true},
+    {id:"mc9",name:"Strawberry Mojito",price:120,veg:true},{id:"mc10",name:"Sunshine Mojito",price:130,veg:true},
+    {id:"mc11",name:"Mango Mojito",price:130,veg:true}
+  ],
+  "Milk Shakes": [
+    {id:"ms1",name:"Badam Shake",price:120,veg:true},{id:"ms2",name:"Vanilla Shake",price:140,veg:true},
+    {id:"ms3",name:"Strawberry Shake",price:140,veg:true},{id:"ms4",name:"Butter Scotch",price:150,veg:true},
+    {id:"ms5",name:"Chocolate Shake",price:150,veg:true},{id:"ms6",name:"Mango Shake",price:150,veg:true},
+    {id:"ms7",name:"Oreo Shake (Dry Fruit)",price:170,veg:true}
+  ],
+  "Ice Creams": [
+    {id:"ic1",name:"Vanilla Ice Cream",price:80,veg:true},{id:"ic2",name:"Strawberry Ice Cream",price:80,veg:true},
+    {id:"ic3",name:"Butter Scotch Ice Cream",price:90,veg:true},{id:"ic4",name:"Chocolate Ice Cream",price:90,veg:true},
+    {id:"ic5",name:"Mango Ice Cream",price:90,veg:true}
+  ],
+  "Beverages": [
+    {id:"bv1",name:"Water Bottle",price:20,veg:true},{id:"bv2",name:"Sweet Pan",price:20,veg:true},
+    {id:"bv3",name:"Thums Up / Sprite (300ml)",price:30,veg:true},{id:"bv4",name:"Butter Milk",price:40,veg:true},
+    {id:"bv5",name:"Fresh Lime Soda Salt",price:60,veg:true},{id:"bv6",name:"Fresh Lime Soda Sweet",price:70,veg:true},
+    {id:"bv7",name:"Sweet Lassi",price:80,veg:true}
+  ]
+};
+module.exports = menu;
